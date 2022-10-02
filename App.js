74f1ba2117dedd3560bbf9946cbafb55dd86e871
Rahul Jha <rahuljha3246@gmail.com>
2022-10-02 16:26:15 +0530

    Push Notification Implemented and App Icon Changed

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import BottomTab from './navigation/MainTabNavigator';
 import {useSelector, useDispatch} from 'react-redux';
 import Auth from './services/Auth';
 import {setLoggedIn, setOwnerLogin} from './actions';
-import Geocoder from 'react-native-geocoding';
+import PushNotification, {Importance} from 'react-native-push-notification';
 const App = () => {
   const Stack = createNativeStackNavigator();
   const {latitude, longitude} = useSelector(state => state.reducer);
@@ -14,6 +14,18 @@ const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
     checkAuthenticate();
+    PushNotification.createChannel(
+      {
+        channelId: '234567890909090909090', // (required)
+        channelName: 'My channel', // (required)
+        channelDescription: 'A channel to categorise your notifications', // (optional) default: undefined.
+        playSound: true, // (optional) default: true
+        soundName: 'notification.mp3', // (optional) See `soundName` parameter of `localNotification` function
+        importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+        vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+      },
+      created => console.log(`createChannel returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+    );
   }, []);
 
   const checkAuthenticate = async () => {
